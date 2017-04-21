db14e02a1bf65d05fa84d5864d303c4456ff255d
Simeon Bird <sbird4@jhu.edu>
2017-04-21 14:57:22 -0400

    Plot changes

--- plot_dm_powers.py
+++ plot_dm_powers.py
@@ -4,6 +4,7 @@ import glob
 import math
 import numpy as np
 import scipy.interpolate
+import re
 import matplotlib
 matplotlib.use('PDF')
 import matplotlib.pyplot as plt
@@ -11,7 +12,7 @@ import matplotlib.pyplot as plt
 datadir = os.path.expanduser("~/data/hybrid-kspace2")
 savedir = "nuplots/"
 sims = ["b300p512nu0.4p","b300p512nu0.4a"]
-zerosims = "b300p512nu0"
+zerosim = "b300p512nu0"
 lss = {"b300p512nu0.4p":"-.", "b300p512nu0.4a":"--"}
 
 def load_genpk(path,box):
@@ -60,10 +61,11 @@ def get_camb_power(matpow):
 def _get_pk(scale, ss):
     """Get the matter power spectrum"""
     sdir = os.path.join(os.path.join(datadir, ss),"output")
-    matpow = glob.glob(os.path.join(sdir,"powerspectrum-"+str(scale)+"*.txt"))
+    matpow = sorted(glob.glob(os.path.join(sdir,"powerspectrum-"+str(scale)+"*.txt")))
     if np.size(matpow) == 0:
         return ([],[])
     matpow = matpow[0]
+    print(matpow)
     return get_camb_power(matpow)
 
 def plot_single_redshift(scale):
@@ -84,14 +86,20 @@ def plot_single_redshift(scale):
 
 def plot_nu_single_redshift(scale):
     """Plot all the neutrino power in simulations at a single redshift"""
-    snap = {0.333:'3', 0.5:'4', 1:'8'}
+    snap = {0.2:'2', 0.333:'4', 0.5:'6', 1:'9'}
     for ss in sims:
         sdir = os.path.join(os.path.join(datadir, ss),"output")
         matpow = glob.glob(os.path.join(sdir,"powerspectrum-nu-"+str(scale)+"*.txt"))
         try:
             (k, pk_nu) = get_nu_power(matpow[0])
         except IndexError:
-            (k, pk_nu) = load_genpk(os.path.join(os.path.join(datadir,ss),"output/PK-nu-PART_00"+snap[scale]),300)
+            try:
+                (k, pk_nu) = load_genpk(os.path.join(os.path.join(datadir,ss),"output/PK-nu-PART_00"+snap[scale]),300)
+                #Shot noise
+                shot=(300/512.)**3*np.ones_like(pk_nu)
+                pk_nu -=shot
+            except FileNotFoundError:
+                continue
         plt.loglog(k, pk_nu,ls=lss[ss], label=ss)
     cambdir = os.path.join(os.path.join(datadir, sims[0]),"camb_linear")
     cambmat = os.path.join(cambdir,"ics_matterpow_"+str(int(1/scale-1))+".dat")
@@ -101,6 +109,30 @@ def plot_nu_single_redshift(scale):
     plt.semilogx(k, rebinned(k),ls=":", label="CAMB")
     plt.legend(loc=0)
     plt.savefig(os.path.join(savedir, "pks-nu-"+str(scale)+".pdf"))
+    plt.clf()
+
+def plot_nu_single_redshift_rel_camb(scale):
+    """Plot all neutrino powers relative to CAMB"""
+    snap = {0.2:'2', 0.333:'4', 0.5:'6', 1:'9'}
+    for ss in sims:
+        cambdir = os.path.join(os.path.join(datadir, sims[0]),"camb_linear")
+        cambmat = os.path.join(cambdir,"ics_matterpow_"+str(int(1/scale-1))+".dat")
+        cambtrans = os.path.join(cambdir,"ics_transfer_"+str(int(1/scale-1))+".dat")
+        (k_nu_camb, pk_nu_camb) = get_camb_nu_power(cambmat, cambtrans)
+        rebinned=scipy.interpolate.interpolate.interp1d(k_nu_camb,pk_nu_camb)
+        sdir = os.path.join(os.path.join(datadir, ss),"output")
+        matpow = glob.glob(os.path.join(sdir,"powerspectrum-nu-"+str(scale)+"*.txt"))
+        try:
+            (k, pk_nu) = get_nu_power(matpow[0])
+        except IndexError:
+            try:
+                (k, pk_nu) = load_genpk(os.path.join(os.path.join(datadir,ss),"output/PK-nu-PART_00"+snap[scale]),300)
+            except FileNotFoundError:
+                continue
+        plt.semilogx(k, pk_nu/rebinned(k),ls=lss[ss], label=ss)
+    plt.ylim(0.9,1.2)
+    plt.legend(loc=0)
+    plt.savefig(os.path.join(savedir, "pks_nu_camb-"+str(scale)+".pdf"))
     plt.clf()
 
 def plot_single_redshift_rel_camb(scale):
@@ -114,46 +146,39 @@ def plot_single_redshift_rel_camb(scale):
         (k_camb, pk_camb) = get_camb_power(camb)
         rebinned=scipy.interpolate.interpolate.interp1d(k_camb,pk_camb)
         plt.semilogx(k, pk/rebinned(k),ls=lss[ss], label=ss)
-    plt.ylim(0.8,1.4)
+    plt.ylim(0.9,1.2)
     plt.legend(loc=0)
     plt.savefig(os.path.join(savedir, "pks_camb-"+str(scale)+".pdf"))
     plt.clf()
 
-def plot_single_redshift_rel_one(scale):
+def plot_single_redshift_rel_one(scale, sims=sims, zerosim=zerosim, ymin=0.5,ymax=1.1,camb=True):
     """Plot all the simulations at a single redshift"""
-    (k_div, pk_div) = _get_pk(scale, zerosims)
-    zerocambdir = os.path.join(os.path.join(datadir, zerosims),"camb_linear")
-    camb = os.path.join(zerocambdir,"ics_matterpow_"+str(int(1/scale-1))+".dat")
-    (zero_k, zero_pk_c) = get_camb_power(camb)
-    zero_reb=scipy.interpolate.interpolate.interp1d(zero_k,zero_pk_c)
-    cambdir = os.path.join(os.path.join(datadir, sims[0]),"camb_linear")
-    cambpath = os.path.join(cambdir,"ics_matterpow_"+str(int(1/scale-1))+".dat")
-    (k_c, pk_c) = get_camb_power(cambpath)
-    plt.semilogx(k_c, pk_c/zero_reb(k_c),ls=":", label="CAMB")
+    (k_div, pk_div) = _get_pk(scale, zerosim)
+    if camb:
+        zerocambdir = os.path.join(os.path.join(datadir, zerosim),"camb_linear")
+        camb = os.path.join(zerocambdir,"ics_matterpow_"+str(int(1/scale-1))+".dat")
+        (zero_k, zero_pk_c) = get_camb_power(camb)
+        zero_reb=scipy.interpolate.interpolate.interp1d(zero_k,zero_pk_c)
+        cambdir = os.path.join(os.path.join(datadir, sims[0]),"camb_linear")
+        cambpath = os.path.join(cambdir,"ics_matterpow_"+str(int(1/scale-1))+".dat")
+        (k_c, pk_c) = get_camb_power(cambpath)
+        plt.semilogx(k_c, pk_c/zero_reb(k_c),ls=":", label="CAMB")
     for ss in sims:
         (k, pk) = _get_pk(scale, ss)
         if np.size(k) == 0:
             continue
         plt.semilogx(k, pk/pk_div,ls=lss[ss], label=ss)
-    if scale > 0.8:
-        (kk,pkk) = load_genpk(os.path.join(os.path.join(datadir,sims[0]),"output/PK-DM-PART_009"),300)
-        (kk,pkknu) = load_genpk(os.path.join(os.path.join(datadir,sims[0]),"output/PK-nu-PART_009"),300)
-        (zk, zpk) = load_genpk(os.path.join(os.path.join(datadir,zerosims),"output/PK-DM-PART_009"),300)
-        plt.semilogx(kk, ((0.3-0.01)*pkk**0.5/0.3+pkknu**0.5*0.01/0.3)**2/zpk, ls="-")
-        (kk,pkk) = load_genpk(os.path.join(os.path.join(datadir,sims[1]),"output/PK-DM-PART_009"),300)
-        matpow = glob.glob(os.path.join(os.path.join(datadir,sims[1]),"output/powerspectrum-nu-"+str(scale)+"*.txt"))
-        (knu, pkknu) = get_nu_power(matpow[0])
-        reb=scipy.interpolate.interpolate.interp1d(knu,pkknu, bounds_error=False)
-        plt.semilogx(kk, ((0.3-0.01)*pkk**0.5/0.3+reb(kk)**0.5*0.01/0.3)**2/zpk, ls="-")
-    plt.ylim(0.5,1.1)
+    plt.ylim(ymin,ymax)
     plt.xlim(1e-2,20)
     plt.legend(loc=0)
-    plt.savefig(os.path.join(savedir, "pks_rel-"+str(scale)+".pdf"))
+    plt.savefig(os.path.join(savedir, "pks_rel-"+str(scale)+str(zerosim[-1])+".pdf"))
     plt.clf()
 
 if __name__ == "__main__":
-    for sc in (0.333, 0.5, 1):
+    for sc in (0.200, 0.333, 0.500, 1):
         plot_nu_single_redshift(sc)
         plot_single_redshift_rel_one(sc)
+        plot_single_redshift_rel_one(sc,sims=[sims[1],],zerosim=sims[0],ymin=0.98,ymax=1.02,camb=False)
         plot_single_redshift_rel_camb(sc)
+        plot_nu_single_redshift_rel_camb(sc)
         plot_single_redshift(sc)
