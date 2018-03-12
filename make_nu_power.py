ebc76601f88d7e83c3ddba0b9a3b390b08260f60
Simeon Bird <sbird@ucr.edu>
2018-03-12 15:14:35 -0700

    Alas, that sort just replaced the array with None, which is why it was so fast

--- make_nu_power.py
+++ make_nu_power.py
@@ -4,6 +4,7 @@ import sys
 import glob
 import numpy
 from nbodykit.lab import BigFileCatalog,FFTPower
+import timeit
 
 def sptostr(sp):
     """Get a string from a species"""
@@ -15,7 +16,12 @@ def sptostr(sp):
 
 def wrapper(a, b):
     "Wrap np.isin so we can specify arguments more explicitly"""
-    return numpy.isin(a,b,assume_unique=True,invert=True)
+    t1 = timeit.default_timer()
+    data= numpy.isin(a,b,assume_unique=True,invert=True)
+    t2 = timeit.default_timer()
+    print("t: ",t2-t1, "shape: ",numpy.shape(a), numpy.shape(b) )
+    return data
+
 
 def compute_fast_power(output, ICS, vthresh=850, Nmesh=1024, species=2, spec2 = None):
     """Compute the compensated power spectrum from a catalogue."""
@@ -23,11 +29,12 @@ def compute_fast_power(output, ICS, vthresh=850, Nmesh=1024, species=2, spec2 = 
     sp2 = sptostr(spec2)
     catnu = BigFileCatalog(output, dataset=str(species)+'/', header='Header')
     outfile = path.join(output,"../power-fast-"+sp+sp2+"-%.4f.txt" % catnu.attrs["Time"][0])
-    if path.isfile(outfile):
-        return
+#     if path.isfile(outfile):
+#         return
     catics = BigFileCatalog(ICS, dataset=str(species)+'/', header='Header')
     fast = (catics['Velocity']**2).sum(axis=1) < vthresh**2/catics.attrs["Time"]**3
-    fastids = catics["ID"][fast].compute().sort()
+    fastids = catics["ID"][fast].compute()
+    fastids.sort()
     #Note: map_blocks runs elementwise over blocks.
     #So we need to pre-compute fastids: if we do not we will
     #end up checking whether elements in a block of catnu["ID"]
@@ -76,8 +83,11 @@ def all_compute(directory):
 def all_fast_compute(directory):
     """Do computation for all snapshots in a directory"""
     snaps = glob.glob(path.join(directory, "output/PART_*"))
+    snaps.sort()
+    snaps.reverse()
     ics = glob.glob(path.join(directory, "ICS/*"))
     for ss in snaps:
+        print(ss)
         compute_fast_power(ss,ics)
         compute_fast_power(ss,ics, spec2=1)
 
